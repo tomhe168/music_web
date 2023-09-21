9dd1edb6d5395d720823c95512c03c2cb45bb439
tomhe168 <zhongjie.he@foxmail.com>
2023-09-21 21:51:45 +0800

    编辑921

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,6 +1,9 @@
 var swiper1 = new Swiper(".mySwiper1", {
+    centeredSlides: true,
+    centeredSlidesBounds:true,
+    centerInsufficientSlides: true,
     slidesPerView: "auto",
-    spaceBetween: 30,
+    // spaceBetween: 30,
     on: {
         slideChange: function() {
             const ids = ['e-collapse1', 'e-collapse2', 'e-collapse3', 'e-collapse4','e-collapse5','e-collapse6'];
@@ -16,6 +19,9 @@ var swiper1 = new Swiper(".mySwiper1", {
 
 
 var swiper2 = new Swiper(".mySwiper2", {
+    centeredSlides: true,
+    centeredSlidesBounds:true,
+    centerInsufficientSlides: true,
     slidesPerView: "auto",
-    spaceBetween: 30,
+    // spaceBetween: 30,
 });
